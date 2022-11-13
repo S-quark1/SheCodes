6163c35c22ea7f1a07e3d3af6c575309f0af51b4
S-quark1 <binarakuanysheva13@gmail.com>
2022-11-13 10:23:28 +0600

    commited project

---
+++ lib/screen/pages/news/pages/newsView.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+import '../models/news.dart';
+
+class NewsViewPage extends StatelessWidget {
+  final News newsPost;
+  const NewsViewPage({Key? key, required this.newsPost}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+          width: size.width,
+          child: Stack(
+            alignment: Alignment.topLeft,
+            children: [
+              ClipRRect(
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage(newsPost.image),
+                  height: size.height,
+                  width: size.width,
+                ),
+              ),
+              Container(
+                width: size.width,
+                height: size.height,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      // const Color(0xCC000000),
+                      Color(0x00000000),
+                      Color(0x00000000),
+                      Color(0xCC000000),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(5),
+                    margin: const EdgeInsets.only(top: 30, left: 10),
+                    child: const Icon(
+                      Icons.arrow_back,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                // alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: size.width,
+                  margin: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text(
+                        newsPost.title,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,),
+                      ),
+                      SizedBox(height: 5.0,),
+                      Row(
+                        children: [
+                          Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image: NetworkImage(newsPost.sourceImage))),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    newsPost.source,
+                                    style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    newsPost.time,
+                                    style: TextStyle(color: Colors.grey[400]),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 40),
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.bookmark_outlined,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                                const Icon(
+                                  Icons.share,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 10, right: 40),
+                        height: 3,
+                        decoration: const BoxDecoration(color: Colors.white),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 10, right: 40),
+                        child: Text(newsPost.descrption,
+                            style: const TextStyle(
+                              color: Colors.white,
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
